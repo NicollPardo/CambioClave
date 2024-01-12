8f2e3d2f363b152ce4ad137020a672ab8409ec66
Nicoll Alexandra <150936297+NicollPardo@users.noreply.github.com>
2024-01-12 15:04:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
-let storedPassword = 'mapasco';
+app.use(cors());
 
-app.use(bodyParser.json());
-
-app.put('/api/cambiar-contrasena', (req, res) => {
-  const { currentPassword, newPassword } = req.body;
-
-  if (currentPassword !== storedPassword) {
-    return res.status(401).json({ success: false, message: 'Contraseña actual incorrecta' });
-  }
-
-  storedPassword = newPassword;
-
-  res.status(200).json({ success: true, message: 'Contraseña cambiada con éxito' });
+app.put('https://ptesa-env-more.eastus.cloudapp.azure.com/k2o/dev/api/api/Usuario/CambiarClave', (req, res) => {
+  res.status(200).json({ message: 'Contraseña cambiada con éxito' });
 });
+
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
